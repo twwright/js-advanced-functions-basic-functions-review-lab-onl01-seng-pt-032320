6954631634d29ac5b41316db62a41f4c71d04a52
Thomas Wright <twwright@gmail.com>
2020-09-25 17:03:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ function wrapAdjective(flair = "*") {
 const Calculator = {
   add: function(a, b) {
     return a + b;
-  };
+  }
  
   subtract: function(a, b) {
     return a - b;
-  };
+  }
  
   multiply: function(a, b) {
     return a * b;
   }
  
-  divide: function(a, b) {
+  let divide = function(a, b) {
     return a / b;
   }
 }
