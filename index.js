691637c28e1590ad0fc277ce2204de952621650c
Thomas Wright <twwright@gmail.com>
2020-09-25 16:56:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ let mondayWork = function(activity = "go to the office") {
   return `This Monday, I will ${activity}.`;
 }
 
+function wrapAdjective(flair = "*") {
+  return function(adj = "special") {
+    return `You are ${flair}${adj}${flair}!`
+  }
+}
